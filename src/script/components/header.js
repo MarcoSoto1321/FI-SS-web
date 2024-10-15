985e7fc7f0208e63c24a40bd5f2913824c220b4d
Moises-Ortega <moises.ortega@fi.unam.edu>
2024-10-15 13:10:29 -0600

    Correccion de rutas para funcionamiento modular

--- src/script/components/header.js
+++ src/script/components/header.js
@@ -11,19 +11,19 @@ class HeaderComponent extends HTMLElement {
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <title>Inicio</title>
-            <link rel="stylesheet" href="/src/css/header.css">
+            <link rel="stylesheet" href="../css/header.css">
         </head>
         <body>
             <header>
                 <div class="encabezado-escudos">
                     <div class="header-content">
                         <div class="izquierda">
-                            <a href="https://www.unam.mx/"><img src="/src/images/Encabezado/UNAM.png" alt="Escudo 1" class="escudo"></img></a>
-                            <a href="https://www.ingenieria.unam.mx/"><img src="/src/images/Encabezado/FI.png" alt="Escudo 2" class="escudo"></img></a>
+                            <a href="https://www.unam.mx/"><img src="../images/Encabezado/UNAM.png" alt="Escudo 1" class="escudo"></img></a>
+                            <a href="https://www.ingenieria.unam.mx/"><img src="../images/Encabezado/FI.png" alt="Escudo 2" class="escudo"></img></a>
                         </div>
                         <h1 class = "titulo-header">Transformación Digital</h1>
                         <div class="derecha">
-                            <img src="/src/images/Encabezado/logo_die2.png" alt="Escudo 3" class="escudo"></img>
+                            <img src="../images/Encabezado/logo_die2.png" alt="Escudo 3" class="escudo"></img>
                         </div>
                     </div>
                 </div>
